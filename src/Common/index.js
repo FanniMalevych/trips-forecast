1b67712d3b36ce4d0484a585d4a86743333eea75
fanni <fmalevych@zoolatech.com>
2024-02-19 00:54:21 +0200

    added city selection and date picker

---
+++ src/Common/index.js
@@ -0,0 +1,2 @@
+export const YOUR_API_KEY = 'CD873TYGD8C3CK5SBJUV5AUA9'
+export const CITIES_LIST = ['London', 'Berlin', 'Tokyo', 'Barcelona', 'Kyiv', 'Paris', 'Milan', 'Stockholm']
--- src/Components/Modal/index.js
+++ src/Components/Modal/index.js
@@ -1,35 +1,37 @@
 import { useState } from 'react';
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
-
+import { CITIES_LIST } from '../../Common/index'
 
 const Modal = ({closeModal}) => {
-function handleSubmit(e) {
-    e.preventDefault();
-    console.log(e.target)
-}
+
 const [date, setDate] = useState(new Date());
 const [startDate, setStartDate] = useState();
 const [endDate, setEndDate] = useState();
+const [selectedCity, setSelectedCity] = useState('')
+
 
 const range = new Date();
 range.setDate(date.getDate() + 15)
-console.log(range);
+// console.log(range);
 
 
+startDate && endDate ? console.log(startDate.toISOString().slice(0, 10), endDate.toISOString().slice(0, 10), selectedCity) : console.log('nothing');
 
-    return (
+
+return (
         <div>
             <p>Create trip</p>
             <button onClick={() => closeModal(false)}>X</button>
-            <form method="post" onSubmit={handleSubmit}>
+            <select
+                onChange={event => setSelectedCity(event.target.value)}
+                defaultValue={selectedCity}
+            >
+                {CITIES_LIST.map((city) => <option value={city}>{city}</option>)}
+            </select>
             <label>
-        Pick a fruit:
-        <select name="selectedFruit">
-          <option value="apple">Apple</option>
-          <option value="banana">Banana</option>
-          <option value="orange">Orange</option>
-        </select>
+        City
+        
       </label>
       <div>
         <label>start date
@@ -55,10 +57,10 @@ console.log(range);
      </label>
     </div>
       <button onClick={() => closeModal(false)}>Cancel</button>
-      <button type="submit">Save</button>
-            </form>
-            
+      <button onClick={() => closeModal(false)}>Save</button>
         </div>
+
+        
     )
 }
 
